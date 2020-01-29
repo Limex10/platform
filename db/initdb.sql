89faef1657d26c05f3639083301dbe38d6e623f4
Emil Persson <peem18cz@student.ju.se>
2020-01-29 17:31:40 +0100

    base completed

---
+++ db/initdb.sql
@@ -0,0 +1,10 @@
+USE `myDB`;
+
+CREATE TABLE `accounts` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(50) NOT NULL,
+    PRIMARY KEY(`id`)
+    
+);
+
--- web_application/src/pl/app.js
+++ web_application/src/pl/app.js
@@ -20,6 +20,21 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response){
   response.render("startScreen.hbs")
 })
+app.get('/createAccount', function(request, response){
+  response.render("createAccount.hbs")
+})
+app.get('/createProfileInfo', function(request, response){
+  response.render("createProfileInfo.hbs")
+})
+app.get('/home', function(request, response){
+  response.render("home.hbs")
+})
+app.get('/viewPerson', function(request, response){
+  response.render("viewPerson.hbs")
+})
+app.get('/yourProfile', function(request, response){
+  response.render("manageProfile.hbs")
+})
 
 app.listen(8080, function(){
   console.log("Web application listening on port 8080.")
