14e2990f5993afa280474f43c443f247e91054df
Emil Persson <peem18cz@student.ju.se>
2020-01-30 13:52:08 +0100

    save

--- db/initdb.sql
+++ db/initdb.sql
@@ -1,57 +1,51 @@
 USE `myDB`;
 
-CREATE TABLE `accounts` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(50) NOT NULL UNIQUE,
-    `password` VARCHAR(50) NOT NULL,
-    PRIMARY KEY(`id`)
+Create TABLE `interests`(
+    `interests_id` INT NOT NULL AUTO_INCREMENT,
+    `interest` VARCHAR(50),
+    PRIMARY KEY (`interests_id`)
+    
 );
 
+
+
 CREATE TABLE `profiles`(
-    `accounts_id` INT,
+    `profile_id` INT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(50) NOT NULL UNIQUE,
+    `password` VARCHAR(50) NOT NULL,
     `city` VARCHAR(50),
     `country` VARCHAR(50),
     `firstname` VARCHAR(50),
     `lastname` VARCHAR(50),
-    `interest1` VARCHAR(50),
-    `interest2` VARCHAR(50),
-    `interest3` VARCHAR(50),
-    `interest4` VARCHAR(50),
-    FOREIGN KEY(accounts_id)
-    REFERENCES accounts(id)
-    ON DELETE CASCADE
+    `id_interest1` INT NOT NULL,
+    `id_interest2` INT NOT NULL,
+    `id_interest3` INT NOT NULL,
+    `id_interest4` INT NOT NULL,
+    FOREIGN KEY (id_interest1)
+    REFERENCES interests(interests_id),
+    FOREIGN KEY (id_interest2)
+    REFERENCES interests(interests_id),
+    FOREIGN KEY (id_interest3)
+    REFERENCES interests(interests_id),
+    FOREIGN KEY (id_interest4)
+    REFERENCES interests(interests_id)
+    ON DELETE CASCADE,
+    PRIMARY KEY(`profile_id`) 
 );
 
-Create TABLE `interests`(
-    `interest1` VARCHAR(50),
-    `interest2` VARCHAR(50),
-    `interest3` VARCHAR(50),
-    `interest4` VARCHAR(50),
-    `interest5` VARCHAR(50),
-    `interest6` VARCHAR(50),
-    `interest7` VARCHAR(50),
-    `interest8` VARCHAR(50),
-    `interest9` VARCHAR(50),
-    `interest10` VARCHAR(50),
-    `interest11` VARCHAR(50),
-    `interest12` VARCHAR(50),
-    `interest13` VARCHAR(50),
-    `interest14` VARCHAR(50),
-    `interest15` VARCHAR(50),
-    `interest16` VARCHAR(50),
-    `interest17` VARCHAR(50),
-    `interest18` VARCHAR(50),
-    `interest19` VARCHAR(50),
-    `interest20` VARCHAR(50)
-);
-
-CREATE TABLE `profilemessage`(
-    `profile_id` INT,
+CREATE TABLE `profilemessages`(
+    `profilemessages_id` INT NOT NULL AUTO_INCREMENT,
     `message` VARCHAR(50),
+    `profile_id` INT NOT NULL,
     FOREIGN KEY(profile_id)
-    REFERENCES accounts(id)
-    ON DELETE CASCADE
+    REFERENCES profiles(profile_id)
+    ON DELETE CASCADE,
+    PRIMARY KEY(`profilemessages_id`)
+    
 );
 
-INSERT INTO interests VALUES ('Gaming','Fishing','Cooking','Cocktails','Coding','Icehockey','Golf','Cars','Traveling','Training',
-'Hiking','Skiing','Horseriding','Hunting','Reading','Movies','Padel','Shopping','FoodLover','Socializing');
+
+
+
+INSERT INTO interests (interest) VALUES ('Gaming'),('Fishing'),('Cooking'),('Cocktails'),('Coding'),('Icehockey'),('Golf'),('Cars'),('Traveling'),('Training'),
+('Hiking'),('Skiing'),('Horseriding'),('Hunting'),('Reading'),('Movies'),('Padel'),('Shopping'),('FoodLover'),('Socializing');
